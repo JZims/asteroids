b8251ae35c38241fa1e7ade2d9691452b6016ef5
JZims <zimmerman.j@hotmail.com>
2025-01-30 20:53:14 -0500

    We have liftoff

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
 import pygame
 from constants import *
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+from player import Player
 
 
     
 
 def main():
     pygame.init()
-
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+
     while True:
-        screen.fill((0, 0, 0))
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        
+        # Update player
+        player.update(dt)
 
-pygame.display.flip()
+        screen.fill((0, 0, 0))
+        player.draw(screen)
+
+        # Limit's frame rate to 60fps
+        dt = clock.tick(60) / 1000
+
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
